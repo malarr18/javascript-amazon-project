4bab71f23238d3be3e03fe089914f1d63347a1a7
malarr18 <malar18012005@gmail.com>
2025-12-17 17:36:17 +0530

    regenerate html when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,8 +7,9 @@ import { deliveryOptions } from "../data/deliveryOptions.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, "days");
-deliveryDate.format("dddd, MMMM D");
+console.log(deliveryDate.format("dddd, MMMM D"));
 
+function renderOrderSummary(){
 let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
@@ -148,5 +149,9 @@ document.querySelectorAll('.js-delivery-option')
     element.addEventListener('click',()=>{
       const {productId,deliveryOptionId}=element.dataset;
       updateDeliveryOption(productId,deliveryOptionId);
+      renderOrderSummary();
     });
   });
+}
+
+renderOrderSummary();
